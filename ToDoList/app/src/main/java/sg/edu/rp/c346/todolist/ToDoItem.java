17773c0916b22e4fa59a706418e0c793b7cd6b91
SHAWN LUAH YEXIANG <17010564@rp.edu.sg>
2018-07-24 16:15:49 +0800

    Initial commit

---
+++ ToDoList/app/src/main/java/sg/edu/rp/c346/todolist/ToDoItem.java
@@ -0,0 +1,43 @@
+package sg.edu.rp.c346.todolist;
+
+import java.util.Calendar;
+
+public class ToDoItem {
+
+    private String title;
+    private Calendar date;
+
+    public ToDoItem(String title, Calendar date) {
+        this.title = title;
+        this.date = date;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Calendar getDate() {
+        return date;
+    }
+
+    public void setDate(Calendar date) {
+        this.date = date;
+    }
+
+    public String getDateString() {
+        String str = date.get(Calendar.DAY_OF_MONTH)+"/"+date.get(Calendar.MONTH)+"/"+date.get(Calendar.YEAR);
+        return str;
+    }
+
+    @Override
+    public String toString() {
+        return "ToDoItem{" +
+                "title='" + title + '\'' +
+                ", date=" + getDateString() +
+                '}';
+    }
+}
